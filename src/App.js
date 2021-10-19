71d3be88f25820ecfb8b54a2136646b75d219322
hasib50115 <hasibur.rahman.50115@gmail.com>
2021-10-19 22:18:41 +0600

    authentication added first one

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Departments from './components/Departments/Departments';
 import Homepage from './components/Homepage/Homepage';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 
 function App() {
   return (
@@ -12,11 +14,20 @@ function App() {
       <BrowserRouter>
         <Header></Header>
         <Switch>
+          <Route exact path="/">
+            <Homepage></Homepage>
+          </Route>
           <Route path="/home">
             <Homepage></Homepage>
           </Route>
           <Route path="/departments">
             <Departments></Departments>
+          </Route>
+          <Route path="/register">
+            <Register></Register>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
           </Route>
         </Switch>
         <Footer></Footer>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import './Header.css';
 
 const Header = () => {
+    const {user, logout} = useFirebase();
     return (
         <div class="bg-light">
             <nav class="navbar navbar-light">
@@ -11,6 +13,10 @@ const Header = () => {
                 <form class="d-flex">
                     <Link className="nav-anchor" to="/home">Home</Link>
                     <Link className="nav-anchor" to="/departments">Departments</Link>
+                    <Link className="nav-anchor" to="/register">Register</Link>
+                    <Link className="nav-anchor" to="/login">Log In</Link>
+                    { user.displayName && <button onClick={logout}>Log Out</button> }
+                    <span>{user.displayName}</span>
                 </form>
                 </div>
             </nav>
